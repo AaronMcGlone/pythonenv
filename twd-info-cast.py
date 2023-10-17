e1c91c557c83f0dd8c1e35d663c0f7047200ad94
AaronMcGlone <aaronjmcglone@gmail.com>
2023-10-17 16:32:03 +0100

    upload testing complete

--- twd-info-cast.py
+++ twd-info-cast.py
@@ -4,7 +4,7 @@ import requests, json
 # The API URL that we'll be requesting from
 url = "https://api.tvmaze.com/shows/73/"
 # Specific resource from the API URL for a/some particular data
-resource = 'episodes'
+resource = 'cast'
 # Build the final host/url to be queried
 host = f'{url}{resource}'
 # Define any parameters
@@ -27,16 +27,6 @@ print(f'{response} {host}')
 json_response = json.loads(response.text)
 print(f'Number of records in array/list (length): {len(json_response)}')
 
-for episode in json_response:# (Array/List: for thing in list)
-    #print(f'{item["number"]}: {item["name"]} (season: {item["season"]}, airdate: {item["airdate"]})')
-    entry_template = {
-        u"PartitionKey": "the-walking-dead",
-        u"RowKey": episode["id"]
-    }
-    entry = dict(entry_template)
-    for i, (k,v) in enumerate(episode.items()):# (Dictionary: for (index) item/s in dict)
-        if isinstance(v,str) or isinstance(v,int):
-            entry.update({k:v})
-        elif k == 'image':
-            entry.update({k:v['medium']})
-    print(entry)
+for item in json_response:
+    print(f'{item["character"]["id"]}: {item["character"]["name"]}')# (season: {item["season"]}, airdate: {item["airdate"]})')
+    
--- twd-upload-cast.py
+++ twd-upload-cast.py
@@ -4,7 +4,7 @@ import requests, json
 # The API URL that we'll be requesting from
 url = "https://api.tvmaze.com/shows/73/"
 # Specific resource from the API URL for a/some particular data
-resource = 'episodes'
+resource = 'cast'
 # Build the final host/url to be queried
 host = f'{url}{resource}'
 # Define any parameters
@@ -27,16 +27,24 @@ print(f'{response} {host}')
 json_response = json.loads(response.text)
 print(f'Number of records in array/list (length): {len(json_response)}')
 
-for episode in json_response:# (Array/List: for thing in list)
+for castItem in json_response:# (Array/List: for thing in list)
     #print(f'{item["number"]}: {item["name"]} (season: {item["season"]}, airdate: {item["airdate"]})')
     entry_template = {
         u"PartitionKey": "the-walking-dead",
-        u"RowKey": episode["id"]
+        u"RowKey": castItem["character"]["id"]
     }
     entry = dict(entry_template)
-    for i, (k,v) in enumerate(episode.items()):# (Dictionary: for (index) item/s in dict)
-        if isinstance(v,str) or isinstance(v,int):
-            entry.update({k:v})
-        elif k == 'image':
-            entry.update({k:v['medium']})
+    for i, (k,v) in enumerate(castItem.items()):
+        #if isinstance(v,str) or isinstance(v,int):
+        if k == 'character':        
+            entry.update({
+                'characterName':v['name'],
+                'characterImage': v['image']['medium']
+            })
+        elif k == 'person':
+            entry.update({
+                'actorName':v['name'],
+                'actorgender': v['gender']
+            
+            })
     print(entry)
