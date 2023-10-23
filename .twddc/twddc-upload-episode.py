b80a69a6e6e42e70dfffaa1de3f98aabd16e0e15
AaronMcGlone <aaronjmcglone@gmail.com>
2023-10-23 12:07:49 +0100

    created a new folder for thewalkingdead dead city

---
+++ .twddc/twddc-upload-episode.py
@@ -0,0 +1,52 @@
+# Import modules that this script/function needs to use
+import requests, json, os
+from azure.data.tables import TableServiceClient
+
+table_name = 'movieepisode'
+conn_str = 'DefaultEndpointsProtocol=https;AccountName=amqt7lcdrg3txzs;AccountKey=ru8DJpku209MHWsh+LIbhpdZySS4b3Tnb1TJQgQPwscGpoDuUCogGM87qMXtcskybChm5olK2HYn+ASt2/Mi/A==;EndpointSuffix=core.windows.net'
+table_service_client = TableServiceClient.from_connection_string(conn_str=conn_str)
+table_client = table_service_client.get_table_client(table_name=table_name)
+
+# The API URL that we'll be requesting from
+url = "https://api.tvmaze.com/shows/60879/"
+# Specific resource from the API URL for a/some particular data
+resource = 'episodes'
+# Build the final host/url to be queried
+host = f'{url}{resource}'
+# Define any parameters
+querystring = {"q":"'walking dead'"}
+# Define any 'body' payload (non in this example)
+payload = ""
+# Headers for the request
+headers = {
+    "Accept": "application/json"
+}
+
+# Create a variable 'response' to contain the response from the API Request
+response = requests.get(host, data=payload, headers=headers, params=querystring)
+print(f'{response} {host}')
+
+#print(response.text)
+# We reviewed data at: https://jsonformatter.curiousconcept.com/
+
+# New variable with the text (data/payload) converted into JSON for processing.
+json_response = json.loads(response.text)
+print(f'Number of records in array/list (length): {len(json_response)}')
+
+entry_count = 0
+for episode in json_response:# (Array/List: for thing in list)
+    #print(f'{item["number"]}: {item["name"]} (season: {item["season"]}, airdate: {item["airdate"]})')
+    entry_template = {
+        u"PartitionKey": "the-walking-dead-dead-city",
+        u"RowKey": str(episode["id"])
+    }
+    entry = dict(entry_template)
+    for i, (k,v) in enumerate(episode.items()):# (Dictionary: for (index) item/s in dict)
+        if isinstance(v,str) or isinstance(v,int):
+            entry.update({k:v})
+        elif k == 'image':
+            entry.update({k:v['medium']})
+    entry_count = entry_count + 1
+    entry = table_client.upsert_entity(entity=entry)
+    print(entry_count)
+    
